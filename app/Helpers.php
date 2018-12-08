37728ff67d02ac6dbfb220e81d4d5bb83278b06c
haoyq <haoyuqi02@gmail.com>
2018-12-08 17:34:59 +0800

    新增冒泡排序

--- app/Helpers.php
+++ app/Helpers.php
@@ -6,3 +6,30 @@
  * Date: 2018/12/8
  */
 
+if (!function_exists('bubbleSort')) {
+    /**
+     * 冒泡排序
+     * @param $arr
+     * @return array|bool
+     */
+    function bubbleSort($arr)
+    {
+        if (!is_array($arr)) {
+            return false;
+        }
+
+        $count = count($arr);
+
+        for ($i = 0; $i < $count; $i++) {
+            for ($j = $i + 1; $j < $count; $j++) {
+                if ($arr[$i] > $arr[$j]) {
+                    $temp = $arr[$j];
+                    $arr[$j] = $arr[$i];
+                    $arr[$i] = $temp;
+                }
+            }
+        }
+
+        return $arr;
+    }
+}
