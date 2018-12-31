ec7b91c9b0a02b57e6998f1bc5c055edd67d04cb
haoyq <haoyuqi02@gmail.com>
2018-12-31 11:24:22 +0800

    实现天气契约

---
+++ app/Service/Weather/Hefeng.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Hefeng.php
+ * 和风天气 API 实现天气契约
+ * Author: haoyq
+ * Date: 2018/12/31
+ */
+
+namespace App\Service\Weather;
+
+use App\Contracts\Weather;
+
+class Hefeng implements Weather
+{
+    public function getWeather($cityName)
+    {
+        $param = ['key' => env('HEFENG_KEY'), 'location' => $cityName];
+        $url = 'https://free-api.heweather.net/s6/weather/now?' . http_build_query($param);
+
+        $jsonData = file_get_contents($url);
+        $arrayData = json_decode($jsonData, true);
+
+        if ($arrayData['HeWeather6'][0]['status'] != 'ok' || !$arrayData) {
+            return '获取天气失败';
+        }
+
+        return $arrayData['HeWeather6'][0]['basic']['location'] . '现在天气' . $arrayData['HeWeather6'][0]['now']['cond_txt'] . '，气温 ' . $arrayData['HeWeather6'][0]['now']['tmp'] . ' 度。';
+    }
+}
