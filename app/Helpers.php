cae2c82203a2fc324fdefd88dd8ec0370ec066f1
haoyq <haoyuqi02@gmail.com>
2019-01-02 18:25:11 +0800

    新增curl封装函数

--- app/Helpers.php
+++ app/Helpers.php
@@ -298,3 +298,55 @@ if (!function_exists('monkey_king')) {
     }
 }
 
+if (!function_exists('curl_request')) {
+    /**
+     * 封装 curl 函数
+     * 根据 https://laravel-china.org/articles/2768/easy-to-use-curl-class 修改
+     * @param $url
+     * @param bool $params
+     * @param bool $isPost
+     * @param bool $isHttps
+     * @return mixed|null
+     */
+    function curl_request($url, $params = false, $isPost = false, $isHttps = false)
+    {
+//        $httpInfo = array();
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36');
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if ($isHttps) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); // 从证书中检查SSL加密算法是否存在
+        }
+        if ($isPost) {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+            curl_setopt($ch, CURLOPT_URL, $url);
+        } else {
+            if ($params) {
+                if (is_array($params)) {
+                    $params = http_build_query($params);
+                }
+                curl_setopt($ch, CURLOPT_URL, $url . '?' . $params);
+            } else {
+                curl_setopt($ch, CURLOPT_URL, $url);
+            }
+        }
+
+        $response = curl_exec($ch);
+
+        if ($response === FALSE) {
+            return null;
+        }
+
+//        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//        $httpInfo = array_merge($httpInfo, curl_getinfo($ch));
+        curl_close($ch);
+
+        return $response;
+    }
+}
+
