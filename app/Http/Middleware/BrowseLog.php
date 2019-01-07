dcbc1b758cc25a1c634e1705786d57ef26fa309f
haoyq <haoyuqi02@gmail.com>
2019-01-07 14:36:30 +0800

    新建中间件记录访问

---
+++ app/Http/Middleware/BrowseLog.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class BrowseLog
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $log = new \App\Models\BrowseLog();
+
+        $log->ip_addr = $request->getClientIp();
+        $log->request_url = $request->path();
+        $log->city_name = get_city_by_ip();
+
+        $log->save();
+
+        return $next($request);
+    }
+}
