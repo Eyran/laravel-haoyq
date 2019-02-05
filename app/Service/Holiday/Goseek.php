eebe56afe9ab415d97186a929f83d73358b3bfaa
haoyq <haoyuqi02@gmail.com>
2019-02-05 19:53:23 +0800

    实现Holiday契约

---
+++ app/Service/Holiday/Goseek.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Goseek.php
+ * 实现 Holiday 契约
+ * Author: haoyq
+ * Date: 2019/02/05
+ */
+
+namespace App\Service\Holiday;
+
+use App\Contracts\Holiday;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendSystemInfo;
+
+
+class Goseek implements Holiday
+{
+    /**
+     * @param $date
+     * @return bool
+     */
+    public function isHoliday($date)
+    {
+        $jsonData = send_request('http://api.goseek.cn/Tools/holiday', ['date' => $date]);
+
+        if (!is_json($jsonData)) {
+            // 发送邮件
+            Mail::to(env('ADMIN_EMAIL'))->send(new SendSystemInfo('获取节日信息失败'));
+            return false;
+        }
+
+        $arr = json_decode($jsonData, true);
+
+        if ($arr['code'] == 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
