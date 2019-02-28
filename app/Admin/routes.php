d8601e57ce839e301a303f79d49cb476241d5c59
haoyq <haoyuqi02@gmail.com>
2019-02-28 22:18:20 +0800

    laravel-admin 新增模块

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -5,11 +5,14 @@ use Illuminate\Routing\Router;
 Admin::registerAuthRoutes();
 
 Route::group([
-    'prefix'        => config('admin.route.prefix'),
-    'namespace'     => config('admin.route.namespace'),
-    'middleware'    => config('admin.route.middleware'),
+    'prefix' => config('admin.route.prefix'),
+    'namespace' => config('admin.route.namespace'),
+    'middleware' => config('admin.route.middleware'),
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index');
 
+    Route::group(['prefix' => 'database', 'namespace' => 'Database'], function ($route) {
+        $route->resource('faker_user', 'FakerUserController');
+    });
 });
--- app/Models/FakerUser.php
+++ app/Models/FakerUser.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class FakerUser extends Model
 {
-    //
+    public static function labels()
+    {
+        return [
+            'id' => '序号',
+            'name' => '姓名',
+            'email' => '邮箱',
+            'age' => '年龄',
+            'city' => '城市',
+            'created_at' => '创建时间',
+            'updated_at' => '更新时间',
+        ];
+    }
 }
